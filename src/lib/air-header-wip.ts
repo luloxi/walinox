f8b2e217abff4b26d6d075829e28255fc188383c
Lulox <lucianoolivabianco@gmail.com>
2026-08-24 13:13:07 -0300

    WIP air header (do not keep)

---
+++ src/lib/air-header-wip.ts
@@ -0,0 +1,3 @@
+import { packEnvelope, unpackEnvelope } from "@/lib/envelope-pack";
+import { looksLikeAirEnvelope } from "@/lib/air-looks";
+import { decodeEnvelope, encodeEnvelope } from "@/lib/payload";
